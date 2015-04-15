8541f9c7a96addb9fc089f7b8f365486809c1403
Franklin S Cooper Jr <fcooper@ti.com>
2015-04-15 07:46:41 -0500

    Initial commit

--- wiki-archiving.py
+++ wiki-archiving.py
@@ -1,87 +1,421 @@
+# -*- coding: utf-8 -*-
+# encoding=utf-8
+
 # Initialize Site object
 import mwclient
 import os
+import re
 import logger
-print os.environ['no_proxy']
+import sys
+import requests
+import urllib
+import os.path
+import collections
+from sys import stdin
+import getpass
 
-site = mwclient.Site(('http','processors.wiki.ti.com'),path='/')
+WikiLink = collections.namedtuple('WikiLink', 'url altname anchor latestrevid wikisyntax')
 
-
-site.login("fcooper", "presario1")
-
-
-page = site.Pages['Sitara_Linux_SDK_CCS_Users_Guide']
-
-thepage =  page.text()
-
-originalpage = page.text()
+WIKI_DOMAINNAME = "processors.wiki.ti.com"
+WIKI_URL = "http://"+WIKI_DOMAINNAME+"/index.php/"
+ARCHIVE_SDK_NAME = "Sitara Linux SDK 08.00.00.00"
+TEMPLATE_WIKI_PAGE = "Template:ArchiveLinks-AMSDKv08.00"
 
 
 
-original = thepage.find("==")
 
-for dog in page.links():
-	#print dog.normalize_title(dog.name)
-	last_rev = -1
-	for rev in dog.revisions():
-		last_rev = rev['revid']
+site = mwclient.Site(('http',WIKI_DOMAINNAME),path='/')
+
+
+def normalize_title(title):
+	# TODO: Make site dependent
+	title = title.strip()
+	if title[0] == ':':
+		title = title[1:]
+	title = title[0].upper() + title[1:]
+	title = title.replace(' ', '_')
+	return title
+
+def getLinks(pageText):
+
+	links = []
+
+	p = re.compile(ur'\[\[.*?\]\]')
+
+	for output in re.findall(p,pageText):
+		if "[[Image:" in output:
+			continue
+		if "[[Category:" in output:
+			continue
+		if "[[:Category:" in output:
+			continue
+		if "[[File:" in output:
+			continue
+		if "[[#" in output:
+			continue
+
+		originalsyntax = output
+
+
+		output = output[2:]
+		output = output[:-2]
+
+		split = output.find("|")
+
+		linkurl = None
+		altname = None
+		anchor = None
+
+		if split is not -1:
+			linkurl = output.split('|',1)[0]
+			altname = output.split('|',1)[1]
+		else:
+			linkurl = output
+
+
+		if linkurl.find("#") is not -1:
+			anchor = linkurl.split("#",1)[1]
+			linkurl = linkurl.split("#",1)[0]
+
+		page2 = site.Pages[normalize_title(linkurl)]
+		last_rev = -1
+		for rev in page2.revisions():
+			last_rev = rev['revid']
+			break
+
+		if last_rev == -1:
+			raise ValueError("Rev ID shouldn't be -1", linkurl)
+
+		links.append(WikiLink(url=linkurl,altname=altname,anchor=anchor,latestrevid=last_rev,wikisyntax=originalsyntax))
+
+	return links
+
+
+def addLinkToTemplate(url,revid,templateText):
+
+	newTemplateURL = "{{#ifeq: {{{linkurl}}} |"+url+"|["+WIKI_URL+"?title="+url+"&oldid="+str(revid)+"{{{anchor}}} {{{altname}}}]|}}"
+
+	verification = newTemplateURL.split("&oldid=")[0]+"&oldid="
+	if verification not in templateText:
+		templateText = templateText + newTemplateURL
+	#else:
+	#	print "String "+url+" already in template"
+
+	return templateText
+
+def updateLinkToTemplate(url,revid,templateText):
+
+	newTemplateURL = "{{#ifeq: {{{linkurl}}} |"+url+"|["+WIKI_URL+"?title="+url+"&oldid="+str(revid)+"{{{anchor}}} {{{altname}}}]|}}"
+
+	first = templateText.find("{{#ifeq: {{{linkurl}}} |"+url+"|["+WIKI_URL+"?title="+url+"&oldid")
+	first = first + len("{{#ifeq: {{{linkurl}}} |"+url+"|["+WIKI_URL+"?title="+url+"&oldid") + 1
+
+	second = templateText[first:].find("{{{anchor}}}") + first
+
+	oldrev = templateText[first:second]
+
+	oldURL = "{{#ifeq: {{{linkurl}}} |"+url+"|["+WIKI_URL+"?title="+url+"&oldid="+str(oldrev)+"{{{anchor}}} {{{altname}}}]|}}"
+
+
+	print "Update"
+
+	return templateText.replace(oldURL,newTemplateURL)
+
+
+def convertLinksToTemplateLinks(pageText,templateText,templateName):
+
+	try:
+		links = getLinks(pageText)
+	except ValueError as err:
+		print "Broken link on page "+ARCHIVE_WIKI_PAGE
+		print(err.args)
+		return
+
+	# neeed to ignore File:
+	for link in links:
+
+		altname = link.altname
+		url = link.url
+		anchor = link.anchor
+		latestrevid = link.latestrevid
+
+		if "File:" in url:
+			print "Ignoring "+url+" since it isn't a link"
+			continue
+
+		if altname is None:
+			if anchor is not None:
+				altname = url+"#"+anchor
+			else:
+				altname = url
+
+
+		pageurl = WIKI_URL+normalize_title(url)+"&oldid="+str(latestrevid)
+
+		templateText = addLinkToTemplate(normalize_title(url),latestrevid,templateText)
+
+		if anchor is None:
+			anchor = ""
+		else:
+			anchor = "#"+anchor
+
+		templateWikiName = templateName.replace("Template:","")
+		newlink = "{{"+templateWikiName+"|linkurl="+normalize_title(url)+"|anchor="+anchor+"|altname="+altname+"}}"
+		pageText = pageText.replace(link.wikisyntax,newlink)
+
+	return (pageText, templateText)
+
+def addArchivedPageWarning(pageText,pageTitle):
+
+	url = "http://processors.wiki.ti.com/index.php/"+normalize_title(pageTitle)
+	archivedMessage = "<blockquote>{{SpCaution|This is the archived page for " + ARCHIVE_SDK_NAME + " if you like to go to the latest version of the page click ["+url+" here]}} </blockquote>"
+
+	return archivedMessage+" \n"+pageText
+
+
+def addArchiveLink(pageTitle,pageText,templateName):
+	last_rev = site.Pages[pageTitle].revisions
+
+	templateWikiName = templateName.replace("Template:","")
+	savedURL = "*{{"+templateWikiName+"|linkurl="+normalize_title(pageTitle)+"|anchor=''|altname=<u>'''"+ARCHIVE_SDK_NAME+"'''</u>}}"
+
+	p = re.compile('=+\040*(?:Archived)+\040*(?:Versions)*\040*=+\n*\r*\040*', re.IGNORECASE)
+
+
+	for m in p.finditer(pageText):
+
+		begin , end = m.span()
+
+		return (True,pageText[:end]+savedURL+"\n"+pageText[(end):])
+
+
+	return (False,savedURL)
+
+def switchImagesToArchiveImages(page,pageText):
+	global site
+
+	switchText = pageText
+
+	p = re.compile('\[\[\040*(?:File|Image)?\040*:.*?\]\]')
+
+	imageStr = []
+
+	archive_prefix = "_"+"archive_"+ARCHIVE_SDK_NAME.replace(" ","_")
+
+	for output in re.findall(p,pageText):
+		#print output
+		imageStr.append(output)
+
+	for image in page.images():
+
+		if "File:" in image.name:
+			name = image.name[5:]
+		else:
+			name = image.name[6:]
+
+		basename = name.rsplit(".",1)[0]
+		ext = name.rsplit(".",1)[1]
+
+		if ext == "zip" or ext == "tar" or ext == "gz" or ext == "pdf":
+			continue
+
+		imageinfo = image.imageinfo
+
+
+		if archive_prefix in basename:
+			print "Already archived image"
+			continue
+
+		saveimage = basename+archive_prefix+"."+ext
+
+
+
+
+
+		if site.Images[saveimage].imageinfo.get('size',None) is None:
+
+			imagedir = "/home/franklin/repositories/git/wiki-utilities/images/"
+			if os.path.isfile(saveimage) is False:
+				urllib.urlretrieve (imageinfo['url'], imagedir+saveimage)
+
+			description = ARCHIVE_SDK_NAME+" archived image"
+			site.upload(open(imagedir+saveimage,'rb'), filename=saveimage,description=description,ignore=True)
+
+			if site.Images[saveimage].imageinfo.get('size',None) is not None:
+				print "Upload successful"
+			else:
+				print "Upload failed"
+
+		#else:
+		#	print "Already uploaded"
+
+
+		matches = filter(lambda x: name in x, imageStr);
+		matches = matches + filter(lambda x: normalize_title(name) in x, imageStr);
+
+		for match in matches:
+			newStr = match.replace(name,saveimage,1)
+			switchText = switchText.replace(match,newStr)
+
+
+	p = re.compile('\[\[\040*(?:File|Image)?\040*:.*?\]\]')
+
+	imageStr = []
+
+	for output in re.findall(p,switchText):
+		if archive_prefix not in output:
+			print "Error image "+output+" has not switched to archive image"
+
+	return switchText
+
+
+
+while True is True:
+	try:
+		print ("Enter Wiki Username: "),
+		username = stdin.readline().strip()
+		password = getpass.getpass(prompt='Wiki Password: ')
+		site.login(username, password)
+
+		TEST_WIKI_PAGE = "ScratchPad-"+username
 		break
-	#print last_rev
-	
-	used_named = dog.name
-	first = thepage.find("[["+used_named,original)
+	except mwclient.errors.LoginError as e:
+		if e[1]['result'] == "WrongPass":
+			print "Incorrect password"
+		elif e[1]['result'] == "NotExists":
+			print "Username does not exist"
 
-	if first is -1:
-		used_named = dog.normalize_title(dog.name)
-		first = thepage.find("[["+used_named,original)
+while True is True:
+	print ("Enter URL: "),
+	url = stdin.readline().strip()
 
-	if first is not -1:
-		second = thepage.find("]]",first)
-		print used_named
-		print second  - (first + len("[["+used_named)) 
-		addmore = ""
-		if  (second - (first + len("[["+used_named))  ) < 10:
-			addmore = "|<u>'''"+used_named+"'''</u>"
-			#print "Doggyyyyyyyyyyyy"
-		
-		thepage = thepage.replace("[["+used_named,"[http://processors.wiki.ti.com/index.php?title="+dog.normalize_title(dog.name)+"&oldid="+str(last_rev)+addmore,first)
-		num = thepage.find("]]",first)
-		thepage = thepage[:num] + thepage[num+1:]
-		
-	#print first
+	url = url.replace("http://processors.wiki.ti.com/index.php/","")
+	url = url.replace("processors.wiki.ti.com/index.php/","")
+	url = url.replace("\n", "")
+
+	if url.find("#") is not -1:
+		url = url.split("#",1)[0]
 
 
-last_rev = -1
-for rev in page.revisions():
-	last_rev = rev['revid']
-	break
-
-page.save(thepage, summary = 'Preparing to archive. Generate direct links')
+	f = open('modified.txt', 'rw+')
 
 
-page = site.Pages['Sitara_Linux_SDK_CCS_Users_Guide']
-last_rev = -1
-for rev in page.revisions():
-	last_rev = rev['revid']
-	break
+	lines = f.read().splitlines()
+
+	if url in lines:
+		print "Already in list"
+		continue
+
+
+	ARCHIVE_WIKI_PAGE = url
+
+
+	print url
+
+	archivepage = site.Pages[ARCHIVE_WIKI_PAGE]
+	testpage = site.Pages[TEST_WIKI_PAGE]
+	templatepage = site.Pages[TEMPLATE_WIKI_PAGE]
+
+	archivedtext = archivepage.text()
+	originaltext = archivepage.text()
+	templatetext = templatepage.text().strip()
+
+
+	print archivepage.revision
+	if archivepage.revision < 1:
+		print "Invalid"
+		continue
+
+
+	# Convert Images to use new Uploaded Archived Images #
+	archivedtext = switchImagesToArchiveImages(archivepage,archivedtext)
+
+
+	# Convert Internal Links to use Template Links #
+	archivedtext , templatetext = convertLinksToTemplateLinks(archivedtext,templatetext,TEMPLATE_WIKI_PAGE)
+
+
+	# Add Archive Link to Template #
+	templatetext = addLinkToTemplate(normalize_title(ARCHIVE_WIKI_PAGE),-1,templatetext)
+
+
+	archivedtext = addArchivedPageWarning(archivedtext,ARCHIVE_WIKI_PAGE)
+
+	testpage.save(archivedtext, summary = 'Archived for '+ARCHIVE_SDK_NAME)
+	templatepage.save(templatetext, summary = 'Preparing to archive. Generate direct links')
+
+
+	print "Pausing to allow page modifications for archived page version.\nGo to "+WIKI_URL+TEST_WIKI_PAGE+" to view"
+
+	print ("Type continue or stop when ready: "),
+
+	shouldContinue = False
+	while True is True:
+		command = stdin.readline().strip()
+
+		if command == "continue" or command == "stop":
+			if command == "continue":
+				shouldContinue = True
+
+			break
+
+		print "Invalid command"
+
+
+	if shouldContinue is False:
+		print "Not making permanent changes"
+		continue
+
+
+	archivedtext =  testpage.text()
+
+	result , value = addArchiveLink(ARCHIVE_WIKI_PAGE,originaltext,TEMPLATE_WIKI_PAGE)
+
+	if result is False:
+		print "Could not find archive links. Please manually add archive links:\n" + value
+	else:
+		originaltext = value
+
+	testpage.save(originaltext, summary = 'Restoring page with original content plus archive link')
+
+
+	print "Pausing to allow page modifications for original page version.\nGo to "+WIKI_URL+TEST_WIKI_PAGE+" to view"
+	print ("Type continue or stop when ready: "),
+
+	shouldContinue = False
+	while True is True:
+		command = stdin.readline().strip()
+
+		if command == "continue" or command == "stop":
+			if command == "continue":
+				shouldContinue = True
+
+			break
+
+		print "Invalid command"
+
+
+	if shouldContinue is False:
+		print "Not making permanent changes"
+		continue
+
+	originaltext = testpage.text()
+
+	archivepage.save(archivedtext, summary = 'Archiving Page')
+
+
+	# Need to call again to get latest values including revision id
+	archivepage = site.Pages[ARCHIVE_WIKI_PAGE]
+
+
+	templatetext = updateLinkToTemplate(normalize_title(ARCHIVE_WIKI_PAGE),archivepage.revision,templatetext)
+
+	templatepage.save(templatetext, summary = 'Preparing to archive. Generate direct links')
+	archivepage.save(originaltext, summary = 'Restoring page with original content plus archive link')
+
+	f.flush()
+	f.close()
 
 
 
-archived = originalpage.find("= Archived versions  =")
-archived = archived + len("= Archived versions  =")+1
 
-sdk = "Sitara SDK 8.0"
-archivedlink = "*[http://processors.wiki.ti.com/index.php?title="+page.normalize_title(page.name)+"&oldid="+str(last_rev)+" <u>'''"+sdk + " -  "+ page.name+" (archived)'''</u>]"
-
-originalpage =  originalpage[:archived] + archivedlink + originalpage[archived:]
-
-
-
-page.save(originalpage, summary = 'Created Archived Links and restoring page')	
-
-	
-#print thepage
-
-#[[Sitara Linux SDK CCS GDB Setup|<u>'''Running GDB Server on CCSv5'''</u>]]
-
-#[http://processors.wiki.ti.com/index.php?title=Sitara Linux SDK CCS GDB Setup&oldid=137210 <u>'''Sitara SDK 05.07 CCSv5 User Guide (archived)'''</u>]
